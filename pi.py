39b88886ba65f6915e44b2e605b11457137854f8
akiajaya <akiajaya1@gmail.com>
2019-08-19 19:33:09 -0400

    this is a update

--- pi.py
+++ pi.py
@@ -1,4 +1,17 @@
 Hello Test
 
+Lets Start again with the learning part
+
+I took a break for a 8 days and I've forgotten everything. 
 
 this is a test on a mac
+
+
+That took a while
+
+    Now the practice begins
+
+
+
+    This is not working
+    
